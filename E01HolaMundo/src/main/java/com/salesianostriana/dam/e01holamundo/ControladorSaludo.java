a87c7fd9875257440481704cc3f1cce3fd2eefc3
admin <mmgmolina03@gmail.com>
2023-04-14 13:31:27 +0200

    Seguimos aprendido

--- E01HolaMundo/src/main/java/com/salesianostriana/dam/e01holamundo/ControladorSaludo.java
+++ E01HolaMundo/src/main/java/com/salesianostriana/dam/e01holamundo/ControladorSaludo.java
@@ -12,8 +12,20 @@ public class ControladorSaludo {
 	public String welcome (@RequestParam(name="nombre", required=false, defaultValue= "Mundo") String nombre, Model model){
 		//verdaderamente solo lleva dos parametros, model es lo que le paso al controlador, @RequestPram es el parametro requerido,
 		//lo que sta entre comillas es como se llama esa variable en el HTML
-		model.addAttribute("nombre", nombre);
-		return "index"; //es el nombre de la pagina HTML que vmos a devolver y no se opone el .HTML
+		model.addAttribute("nombre", nombre);//lo que sta entre comillas es como se llama esa variable en el HTML y lo que estsa sin comillas es el valor
+		return "index"; //es el nombre de la pagina HTML que vmos a devolver y no se pone el .HTML y entre comillas 
 		
 		}
+	@GetMapping("/saludo2")
+	public String welcome2(Model model) {
+		model.addAttribute("persona", new Persona("Manuel", "Molina García"));
+		return "saludoPersonalizado";
+	}
+	@GetMapping ("/saludo3")
+	public String welcome3(Model model) {
+	model.addAttribute("saludo", "Hola Mundo");/*Variable tipo text normal*/
+	model.addAttribute("mensaje", "¡Se me está haciendo largo el proyecto final!");
+	model.addAttribute("url", "https://triana.salesianos.edu");/*devolvemos una url*/
+	return "saludoYEnlace";
+	}
 }
---
+++ E01HolaMundo/src/main/java/com/salesianostriana/dam/e01holamundo/Persona.java
@@ -0,0 +1,16 @@
+package com.salesianostriana.dam.e01holamundo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Persona {
+	
+	private String nombre;
+	private String apellidos;
+
+}
